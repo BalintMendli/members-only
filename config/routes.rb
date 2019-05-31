8ccbf745ab463f32cc3c8b7b5e15fa065ceb4a1e
zenott <mblevel@gmail.com>
2019-05-31 18:22:35 +0200

    Add posts controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/sign_in', to: 'sessions#new', as: 'sign_in'
   post '/sign_in', to: 'sessions#create'
   delete '/sign_out', to: 'sessions#delete'
+  resources :posts, only: [:new, :create, :index]
 end
