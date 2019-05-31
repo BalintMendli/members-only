806c930d0446be102d941c2ee621ee93da45a468
zenott <mblevel@gmail.com>
2019-05-31 16:31:56 +0200

    Add remember digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
+  before_create :set_remember_digest
+
   has_secure_password
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def set_remember_digest
+    self.remember_digest = User.digest(User.new_token)
+  end
 end
