5bcf52361ff45e094719111638711c5011cd5afb
zenott <mblevel@gmail.com>
2019-05-31 16:04:22 +0200

    Add sign in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/sign_in', to: 'sessions#new', as: 'sign_in'
 end
