1c03265a9b7f14170d3100f1cd6c97f822031bc5
zenott <mblevel@gmail.com>
2019-05-31 16:02:23 +0200

    Add sessions controller

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_05_31_133353) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
