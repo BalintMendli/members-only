0785c14e033348ae2fddf2f7a2e876f2d8cad94b
zenott <mblevel@gmail.com>
2019-06-03 22:32:12 +0200

    Add sign up functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,14 @@ class PostsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create]
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
+      flash[:success] = 'Post saved!'
       redirect_to posts_path
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'posts#index'
   get '/sign_in', to: 'sessions#new'
   post '/sign_in', to: 'sessions#create'
   delete '/sign_out', to: 'sessions#delete'
   resources :posts, only: [:new, :create, :index]
+  resources :users, only: [:new, :create]
 end
